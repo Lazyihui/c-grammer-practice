e18eafffc6eca89bb2f1bed5105d231107886041
包子 <chengbaozi6@qq.com>
2024-01-24 19:31:25 +0800

    m

--- m19.12.cs
+++ m19.12.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 
-public class HelloWorld
+namespace Ex
 {
-    public static void Main()
+
+    public class HelloWorld
     {
-        Console.WriteLine("hello world");
-        Console.ReadKey();
+        double length;
+        double width;
+
+        public void Init()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public void Display()
+        {
+            Console.WriteLine("lenth:{0}", length.ToString());
+            Console.WriteLine("width:{0}", width);
+
+        }
+    }
+    public class ExMain
+    {
+        public static void Main()
+        {
+            HelloWorld he = new HelloWorld();
+            he.Display();
+            Console.ReadLine();
+        }
     }
 }
